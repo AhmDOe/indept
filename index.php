36b1961536ba88907ea8635222179c7938dd6450
Ernest <asieduernest@yahoo.co.uk>
2021-09-25 16:44:49 -0400

    link style

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 include("data_mapper.php"); ?>
 
 <head>
-  <title> Student Report Card </title>
-  <h2> Student Report Card </h2>
+  <title>Student Report Card</title>
+  <link rel="stylesheet" href="style.css" />
 </head>
+<h2>Student Report Card</h2>
 
 <body>
-
   <div class="row">
     <form method="get" class="report_form">
       <label for="student_id">Student ID</label>
-      <input id="student_id" required name='student_id' type="number">
-      <button type="submit" name="GET_REPORT_ONE" value='true'>Get Report</button>
+      <input id="student_id" required name="student_id" type="number" />
+      <button type="submit" name="GET_REPORT_ONE" value="true">Get Report</button>
     </form>
 
     <form action="" method="get" class="report_form">
-      <button name="GET_REPORT_ALL">Get All Reports</button>
+      <button name="GET_REPORT_ALL" value='true'>Get All Reports</button>
     </form>
     <form action="" method="get" class="report_form">
       <button>Clear</button>
@@ -31,11 +31,9 @@ include("data_mapper.php"); ?>
       fetchStudentInfoLargeStmt($db_conn, $_GET['student_id'])->fetchAll()
     );
   } else if (isset($_GET['GET_REPORT_ALL'])) {
-    echo generateStudentsReportMarkup($db_conn);
-  }
-  ?>
-
-
+    echo
+    generateStudentsReportMarkup($db_conn);
+  } ?>
 </body>
 
 </html>
